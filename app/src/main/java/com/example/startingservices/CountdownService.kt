c99e37dace47cbffe0a49c97d3fd754e9d0f0f7c
Malvin Prifti <mprifti34@gmail.com>
2023-11-13 15:32:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/startingservices/CountdownService.kt
@@ -0,0 +1,5 @@
+package com.example.startingservices
+
+class CountdownService {
+
+}
---
+++ app/src/main/java/com/example/startingservices/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.startingservices
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+
+        startButton.setOnClickListener {
+
+        }
+    }
+}
